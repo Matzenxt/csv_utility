d6a1c7013570352cb918361f23ecdc32bf06fb85
Matthias Lodner <matthias.lodner@uni-ulm.de>
2020-09-24 00:28:58 +0200

    Added dependencies and seahorse quickstart demo.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use seahorse::{App};
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let app = App::new(env!("CARGO_PKG_NAME"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .usage("cli [args]")
+        .action(|c| println!("Hello, {:?}", c.args.get(0).unwrap()));
+
+    app.run(args);
 }
