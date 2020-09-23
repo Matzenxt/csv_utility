8d14faf3ac85f2a0c2fd7adade67266e53f75177
Matzenxt <matthias.lodner@web.de>
2020-09-24 01:49:05 +0200

    Add commands.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use seahorse::{App};
+use seahorse::{App, Command, Context};
 use std::env;
 
 fn main() {
@@ -7,7 +7,46 @@ fn main() {
         .author(env!("CARGO_PKG_AUTHORS"))
         .version(env!("CARGO_PKG_VERSION"))
         .usage("cli [args]")
-        .action(|c| println!("Hello, {:?}", c.args.get(0).unwrap()));
+        .action(|c| println!("Hello, {:?}", c.args.get(0).unwrap()))
+        .command(map_command())
+        .command(rm_empty_rows_command())
+        .command(rm_rows_with_threshold_command());
 
     app.run(args);
+}
+
+fn map_command() -> Command {
+    Command::new("map")
+        .alias("m")
+        .usage("Maps entries from source file to header from header file and saves to output file [source file] [new header] [outputfile]")
+        .action(map_csv)
+}
+
+fn map_csv(c: &Context) {
+    // TODO: Impl.
+    println!("Map csv");
+}
+
+fn rm_empty_rows_command() -> Command {
+    Command::new("rmer")
+        .alias("rer")
+        .usage("Removes empty rows from csv file [input file] [outputfile]")
+        .action(rm_empty_rows)
+}
+
+fn rm_empty_rows(c: &Context) {
+    // TODO: Impl.
+    println!("Remove empty rows");
+}
+
+fn rm_rows_with_threshold_command() -> Command {
+    Command::new("asdf")
+        .alias("rrwt")
+        .usage("Remove rows with less than 'x' entries [source file] [outputfile] [x]")
+        .action(rm_rows_with_threshold)
+}
+
+fn rm_rows_with_threshold(c: &Context) {
+    // TODO: Impl.
+    println!("Remove rows with less than 'x' entries");
 }
