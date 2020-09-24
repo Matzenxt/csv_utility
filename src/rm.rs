eaeed2d431cd53960ffed4f10dad19bb67b1bdcf
Matthias Lodner <matthias.lodner@uni-ulm.de>
2020-09-24 22:39:29 +0200

    Added functionality to remove empty rows.

--- src/rm.rs
+++ src/rm.rs
@@ -5,8 +5,58 @@ use std::path::Path;
 use std::num::ParseIntError;
 
 pub fn remove_empty_rows(c: &Context) {
-    // TODO: Impl.
-    println!("Remove empty rows");
+    if c.args.len() != 2 {
+        eprintln!("Please check your arguments");
+        std::process::exit(0);
+    }
+
+    let source_file: File = match Path::new(c.args.get(0).unwrap()).exists() {
+        true => {
+            File::open(c.args.get(0).unwrap()).unwrap()
+        }
+        false => {
+            eprintln!("Source file: '{}' does not exists!", c.args.get(0).unwrap());
+            std::process::exit(0);
+        }
+    };
+
+    let output_file: File = match File::create(c.args.get(1).unwrap()) {
+        Ok(file) => {
+            file
+        }
+        Err(_) => {
+            eprintln!("Could not create output file: '{}'!", c.args.get(1).unwrap());
+            std::process::exit(0);
+        }
+    };
+
+    let mut writer: Writer<File> = csv::Writer::from_writer(output_file);
+    let mut reader: Reader<File> = ReaderBuilder::new().delimiter(b';').from_reader(source_file);
+
+    let mut added_row_counter: usize = 0;
+    let mut removed_row_counter: usize = 0;
+
+    for record in reader.records() {
+        let record: StringRecord = record.unwrap();
+
+        let mut entity_counter: usize = 0;
+
+        for i in 0..record.len() {
+            if !record[i].is_empty() {
+                entity_counter = entity_counter + 1;
+            }
+        }
+
+        if entity_counter == 0 {
+            writer.write_record(&record);
+            added_row_counter = added_row_counter + 1;
+        } else {
+            removed_row_counter = removed_row_counter + 1;
+        }
+    }
+
+    writer.flush();
+    println!("Removed {} from {} lines , {} lines remaining.", removed_row_counter, (added_row_counter + removed_row_counter), added_row_counter);
 }
 
 pub fn remove_rows_with_threshold(c: &Context) {
