dc2ed41dea28f38889c029d4fc64f61c25a5f38b
Matzenxt <matthias.lodner@web.de>
2020-09-30 11:27:48 +0200

    Read header from file.

--- src/mapper/menu.rs
+++ src/mapper/menu.rs
@@ -2,9 +2,31 @@ use seahorse::Context;
 use console::{Term, Style};
 use dialoguer::theme::{ColorfulTheme, Theme};
 use dialoguer::Select;
-use crate::mapper::data::test;
+use std::fs::File;
+use crate::util::{get_source_file, create_output_file};
+use csv::{Reader, Writer, ReaderBuilder, StringRecord};
 
 pub fn main(c: &Context) {
+    if c.args.len() != 3 {
+        eprintln!("Please check your arguments");
+        std::process::exit(0);
+    }
+
+    let source_file: File = get_source_file(c, 0);
+    let dest_file: File = get_source_file(c, 1);
+    let output_file: File = create_output_file(c, 2);
+
+    let mut reader_source: Reader<File> = ReaderBuilder::new().delimiter(b';').from_reader(source_file);
+    let mut reader_dest: Reader<File> = ReaderBuilder::new().delimiter(b';').from_reader(dest_file);
+    let mut writer_output: Writer<File> = csv::Writer::from_writer(output_file);
+
+    // Read dest header to vec
+    let mut dest_headers: Vec<String> = Vec::new();
+    let headers = reader_dest.headers().unwrap();
+    for entry in headers {
+        dest_headers.push(entry.to_string());
+    }
+
     let term = Term::stdout();
     term.set_title("CSV mapper");
 
@@ -14,7 +36,7 @@ pub fn main(c: &Context) {
     };
 
     loop {
-        term.clear_screen();
+        //term.clear_screen();
 
         let next_menu = Select::with_theme(&theme)
             .with_prompt("Choose action")
@@ -66,4 +88,8 @@ fn map_view(term: &Term, theme: &ColorfulTheme) {
             }
         }
     }
+}
+
+fn get_headers_from_file(reader: Reader<File>) -> Vec<String> {
+
 }
