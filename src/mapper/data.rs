8c30a5f5077ea129009656438f36257e766c5d1f
Matzenxt <matthias.lodner@web.de>
2020-09-30 11:27:31 +0200

    Start adding data structure for mapping.

--- src/mapper/data.rs
+++ src/mapper/data.rs
@@ -1,1 +1,44 @@
 // TODO: Add data structure for mapping
+
+use std::borrow::BorrowMut;
+
+pub struct HeaderMap {
+    maps: Vec<Map>,
+}
+
+pub struct Map {
+    dest_entry: HeaderEntry,
+    source_entry: Option<HeaderEntry>,
+}
+
+pub struct HeaderEntry {
+    name: String,
+    position: usize,
+}
+
+impl HeaderMap {
+    pub fn new(dest_headers: Vec<String>) -> HeaderMap {
+        let mut temp: Vec<Map> = vec![];
+
+        let mut pos_counter: usize = 0;
+        for dest_header in dest_headers {
+            temp.push(Map::new(pos_counter, dest_header));
+        }
+
+        HeaderMap {
+            maps: temp,
+        }
+    }
+}
+
+impl Map {
+    pub fn new(dest_pos: usize, dest_name: String) -> Map {
+        Map {
+            dest_entry: HeaderEntry {
+                name: dest_name,
+                position: dest_pos
+            },
+            source_entry: None
+        }
+    }
+}
